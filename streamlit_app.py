90d7f62fc1e74ffa130a540a230d386161a983fc
arnoldtRealph <127623684+arnoldtRealph@users.noreply.github.com>
2023-04-08 20:09:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🎈 Presentation App')
-components.iframe("https://docs.google.com/presentation/d/1bMI6D6azSv5LkPCQ5EQQSi0hx-DQLVbuk4itbC06xmQ/edit?usp=sharing" frameborder="0" width="1440", height=432)
+components.iframe("https://docs.google.com/presentation/d/1bMI6D6azSv5LkPCQ5EQQSi0hx-DQLVbuk4itbC06xmQ/edit?usp=sharing", height=432)
